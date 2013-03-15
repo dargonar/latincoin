f59038849edb72562d3494adcfe84b1b9ed58d96
Pablo <ptutino@gmail.com>
2013-03-14 22:08:17 -0300

    separacao de forms de la vista profile.

--- appengine/assets/js/app.js
+++ appengine/assets/js/app.js
@@ -2071,7 +2071,109 @@ var App = function () {
         jQuery('#sample_3_wrapper .dataTables_length select').addClass("m-wrap xsmall"); // modify table per page dropdown
     }
 
-    var handleEditableTables = function () {
+    var handleTablaBitcoins = function () {
+
+        function restoreRow(oTable, nRow) {
+            var aData = oTable.fnGetData(nRow);
+            var jqTds = $('>td', nRow);
+
+            for (var i = 0, iLen = jqTds.length; i < iLen; i++) {
+                oTable.fnUpdate(aData[i], nRow, i, false);
+            }
+
+            oTable.fnDraw();
+        }
+
+        function editRow(oTable, nRow) {
+            var aData = oTable.fnGetData(nRow);
+            var jqTds = $('>td', nRow);
+            jqTds[0].innerHTML = '<input type="text" class="m-wrap small" value="' + aData[0] + '">';
+            //jqTds[1].innerHTML = '<input type="text" class="m-wrap small" value="' + aData[1] + '">';
+            jqTds[2].innerHTML = '<a class="edit" href="">Guardar</a>&nbsp;<a class="cancel" href="">Cancelar</a>';
+        }
+        
+        function saveRow(oTable, nRow) {
+            var jqInputs = $('input', nRow);
+            oTable.fnUpdate(jqInputs[0].value, nRow, 0, false);
+            oTable.fnUpdate(jqInputs[1].value, nRow, 1, false);
+            oTable.fnUpdate('<a class="edit" href="">Editar</a>', nRow, 2, false);
+            oTable.fnDraw();
+        }
+
+         function cancelEditRow(oTable, nRow) {
+            var jqInputs = $('input', nRow);
+            oTable.fnUpdate(jqInputs[0].value, nRow, 0, false);
+            oTable.fnUpdate(jqInputs[1].value, nRow, 1, false);
+            oTable.fnUpdate('<a class="edit" href="">Editar</a>', nRow, 2, false);
+            oTable.fnDraw();
+        }
+
+        var oTable = $('#tabla_direcciones_bitcoin').dataTable();
+        jQuery('#tabla_direcciones_bitcoin_new_wrapper .dataTables_filter input').addClass("m-wrap medium"); // modify table search input
+        jQuery('#tabla_direcciones_bitcoin_new_wrapper .dataTables_length select').addClass("m-wrap xsmall"); // modify table per page dropdown
+
+        var nEditing = null;
+
+        $('#tabla_direcciones_bitcoin_new').click(function (e) {
+            e.preventDefault();
+            var aiNew = oTable.fnAddData(['', '',
+                '<a class="edit" href="">Editar</a>&nbsp;<a class="cancel" data-mode="new" href="">Cancelar</a>']);
+            var nRow = oTable.fnGetNodes(aiNew[0]);
+            editRow(oTable, nRow);
+            nEditing = nRow;
+        });
+
+        /*
+        $('#tabla_direcciones_bitcoin a.delete').live('click', function (e) {
+            e.preventDefault();
+
+            if (confirm("Are you sure to delete this row ?") == false) {
+                return;
+            }
+
+            var nRow = $(this).parents('tr')[0];
+            oTable.fnDeleteRow(nRow);
+            alert("Deleted! Do not forget to do some ajax to sync with backend :)");
+        });
+        */
+
+        $('#tabla_direcciones_bitcoin_new a.cancel').live('click', function (e) {
+            e.preventDefault();
+            if ($(this).attr("data-mode") == "new") {
+                var nRow = $(this).parents('tr')[0];
+                oTable.fnDeleteRow(nRow);
+            } else {
+                restoreRow(oTable, nEditing);
+                nEditing = null;            
+            }
+        });
+
+        $('#tabla_direcciones_bitcoin_new a.edit').live('click', function (e) {
+            e.preventDefault();
+
+            /* Get the row as a parent of the link that was clicked on */
+            var nRow = $(this).parents('tr')[0];
+
+            if (nEditing !== null && nEditing != nRow) {
+                /* Currently editing - but not this row - restore the old before continuing to edit mode */
+                restoreRow(oTable, nEditing);
+                editRow(oTable, nRow);
+                nEditing = nRow;
+            } else if (nEditing == nRow && this.innerHTML == "Save") {
+                /* Editing this row and want to save it */
+                saveRow(oTable, nEditing);
+                nEditing = null;
+                alert("Updated! Do not forget to do some ajax to sync with backend :)");
+            } else {
+                /* No edit in progress - let's start one */
+                editRow(oTable, nRow);
+                nEditing = nRow;
+            }
+        });
+    }
+
+    
+    var handleTablaCuentasBancarias = function () {
 
         function restoreRow(oTable, nRow) {
             var aData = oTable.fnGetData(nRow);
@@ -2091,8 +2193,21 @@ var App = function () {
             jqTds[1].innerHTML = '<input type="text" class="m-wrap small" value="' + aData[1] + '">';
             jqTds[2].innerHTML = '<input type="text" class="m-wrap small" value="' + aData[2] + '">';
             jqTds[3].innerHTML = '<input type="text" class="m-wrap small" value="' + aData[3] + '">';
-            jqTds[4].innerHTML = '<a class="edit" href="">Save</a>';
-            jqTds[5].innerHTML = '<a class="cancel" href="">Cancel</a>';
+            jqTds[4].innerHTML = getCuentaCombo(aData[4]); //'<input type="text" class="m-wrap small" value="' + aData[4] + '">';
+            
+            jqTds[5].innerHTML = '<a class="edit" href="">Guardar</a>&nbsp;<a class="cancel" href="">Cancelar</a>';
+        }
+        
+        function getCuentaCombo(value){
+          if(value=='Cuenta Corriente')
+          {
+            return '<select class="medium m-wrap" tabindex="1"><option value="1" selected>Cuenta Corriente</option><option value="2">Caja de Ahorro</option></select>';
+          }
+          if(value=='Caja de Ahorro')
+          {
+            return '<select class="medium m-wrap" tabindex="1"><option value="1">Cuenta Corriente</option><option value="2" selected>Caja de Ahorro</option></select>';
+          }
+          return '<select class="medium m-wrap" tabindex="1"><option value="1">Cuenta Corriente</option><option value="2">Caja de Ahorro</option></select>';
         }
 
         function saveRow(oTable, nRow) {
@@ -2101,8 +2216,8 @@ var App = function () {
             oTable.fnUpdate(jqInputs[1].value, nRow, 1, false);
             oTable.fnUpdate(jqInputs[2].value, nRow, 2, false);
             oTable.fnUpdate(jqInputs[3].value, nRow, 3, false);
-            oTable.fnUpdate('<a class="edit" href="">Edit</a>', nRow, 4, false);
-            oTable.fnUpdate('<a class="delete" href="">Delete</a>', nRow, 5, false);
+            //oTable.fnUpdate(jqInputs[4].value, nRow, 4, false);
+            oTable.fnUpdate('<a class="edit" href="">Editar</a>&nbsp;<a class="delete" href="">Borrar</a>', nRow, 5, false);
             oTable.fnDraw();
         }
 
@@ -2112,26 +2227,27 @@ var App = function () {
             oTable.fnUpdate(jqInputs[1].value, nRow, 1, false);
             oTable.fnUpdate(jqInputs[2].value, nRow, 2, false);
             oTable.fnUpdate(jqInputs[3].value, nRow, 3, false);
-            oTable.fnUpdate('<a class="edit" href="">Edit</a>', nRow, 4, false);
+            //oTable.fnUpdate(jqInputs[4].value, nRow, 4, false);
+            oTable.fnUpdate('<a class="edit" href="">Editar</a>', nRow, 5, false);
             oTable.fnDraw();
         }
 
-        var oTable = $('#sample_editable_1').dataTable();
-        jQuery('#sample_editable_1_wrapper .dataTables_filter input').addClass("m-wrap medium"); // modify table search input
-        jQuery('#sample_editable_1_wrapper .dataTables_length select').addClass("m-wrap xsmall"); // modify table per page dropdown
+        var oTable = $('#tabla_cuentas_bancarias').dataTable();
+        jQuery('#tabla_cuentas_bancarias_wrapper .dataTables_filter input').addClass("m-wrap medium"); // modify table search input
+        jQuery('#tabla_cuentas_bancarias_wrapper .dataTables_length select').addClass("m-wrap xsmall"); // modify table per page dropdown
 
         var nEditing = null;
 
-        $('#sample_editable_1_new').click(function (e) {
+        $('#tabla_cuentas_bancarias_new').click(function (e) {
             e.preventDefault();
-            var aiNew = oTable.fnAddData(['', '', '', '',
-                '<a class="edit" href="">Edit</a>', '<a class="cancel" data-mode="new" href="">Cancel</a>']);
+            var aiNew = oTable.fnAddData(['', '', '', '', '',
+                '<a class="edit" href="">Editar</a>&nbsp;<a class="cancel" data-mode="new" href="">Cancelar</a>']);
             var nRow = oTable.fnGetNodes(aiNew[0]);
             editRow(oTable, nRow);
             nEditing = nRow;
         });
 
-        $('#sample_editable_1 a.delete').live('click', function (e) {
+        $('#tabla_cuentas_bancarias a.delete').live('click', function (e) {
             e.preventDefault();
 
             if (confirm("Are you sure to delete this row ?") == false) {
@@ -2143,7 +2259,7 @@ var App = function () {
             alert("Deleted! Do not forget to do some ajax to sync with backend :)");
         });
 
-        $('#sample_editable_1 a.cancel').live('click', function (e) {
+        $('#tabla_cuentas_bancarias a.cancel').live('click', function (e) {
             e.preventDefault();
             if ($(this).attr("data-mode") == "new") {
                 var nRow = $(this).parents('tr')[0];
@@ -2154,7 +2270,7 @@ var App = function () {
             }
         });
 
-        $('#sample_editable_1 a.edit').live('click', function (e) {
+        $('#tabla_cuentas_bancarias a.edit').live('click', function (e) {
             e.preventDefault();
 
             /* Get the row as a parent of the link that was clicked on */
@@ -2792,8 +2908,9 @@ var App = function () {
                 handlKnobElements()
             }
 
-            if (App.isPage("table_editable")) {
-                handleEditableTables(); // handle editable tables
+            if (App.isPage("profile")) {
+                handleTablaCuentasBancarias(); // handle editable tables
+                handleTablaBitcoins(); // handle editable tables
             }
 
             if (App.isPage("table_managed")) {
